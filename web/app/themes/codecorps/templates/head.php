ca326dac248520805d144f0bf1fa593aa410cc05
Cody Baker <cody@flickerbox.com>
2016-11-11 17:03:07 -0800

    Remove extra title tag
    Sage theme supports WP 4.1 title-tag feature which resulted in a duplicate title tag

--- web/app/themes/codecorps/templates/head.php
+++ web/app/themes/codecorps/templates/head.php
@@ -1,7 +1,6 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?php wp_title(); ?></title>
   <meta name="description" content="Contribute to software projects for social good.">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
